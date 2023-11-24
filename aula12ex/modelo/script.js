8f49b716f9fd8cf2728390dbf912bc8776257d99
sebasttia <144245136+sebasttia@users.noreply.github.com>
2023-11-23 20:04:43 -0400

    add

--- aula12ex/modelo/script.js
+++ aula12ex/modelo/script.js
@@ -1,1 +1,14 @@
-alert('Olá !')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = 14
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'manha.png'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'tarde.png'
+    } else {
+        img.src = 'noite.png'
+    }
+}
